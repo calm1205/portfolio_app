3350140112073eb069560ee4e201df122c6b181f
tkgcysk <yusaku.takiguchi@di-v.co.jp>
2020-08-11 15:16:40 +0900

    configure before_action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,18 @@
 class ProductsController < ApplicationController
+  # before_action :admin_user?, only: [:new, :create]
+  # before_action :set_product, only: [:show, :purchace_confirmaition]
+  # skip_before_action :authenticated_user, only: [:index, :show]
   def index
   end
+
+  # private
+  # def admin_user?
+  #   redirect_to root_path, unless current_user.admin?
+  #    end
+  # end
+
+  # def set_product
+  #   @product = Product.find(params[:id])
+  # end
+
 end
