855fc41ab842b1599aea38a5cfb7a33a96a3b2a1
calm1205 <naginani072@gmail.com>
2020-08-12 11:15:37 +0900

    set devise routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+  }
+
+  devise_scope :user do
+    get 'users/new_address' => 'users/registrations#new_address'
+    post 'users/create_address' => 'users/registrations#create_address'
+  end
 end
