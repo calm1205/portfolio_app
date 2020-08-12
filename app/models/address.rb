e2a81b796592f978da94dcb2693dcb374047f992
calm1205 <naginani072@gmail.com>
2020-08-12 11:11:39 +0900

    set association

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ApplicationRecord
+
+  # association
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # association
+  has_one :address, dependent: :destroy
 end
