32b277cf2a2785b0cbe3b9fa562b004cd8e6337a
calm1205 <naginani072@gmail.com>
2020-08-23 10:58:44 +0900

    make subsucription function

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,6 +7,8 @@ class Card < ApplicationRecord
   validates :card_token, :customer_token, presence: true
 
   # method
+  
+  # カード情報の取得
   def self.get_card(user)
     Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
     customer_token = user.card.customer_token
@@ -14,4 +16,20 @@ class Card < ApplicationRecord
     customer = Payjp::Customer.retrieve(customer_token)
     card_data = customer.cards[:data].first
   end
+
+  # 有料会員の定期支払い
+  def self.subscription
+    members = User.where(member: 1)
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+
+    members.each do |member|
+      customer_token = member.card.customer_token
+      Payjp::Charge.create(
+        amount: 5000,
+        customer: customer_token,
+        currency: "jpy"
+      )
+    end
+  end
+
 end
