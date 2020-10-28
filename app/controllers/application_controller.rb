e4735e32a27cb2555bb21c9f0772b8e458c628e7
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2020-10-28 13:15:38 +0900

    rafactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :configure_account_update_parameters, if: :devise_controller?
 
-  helper_method :current_cart
-
-  def current_cart
-    Cart.find(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound
-    cart = Cart.create
-    session[:cart_id] = cart.id
-    return cart
-  end
-
-  # def current_cart
-  #   if @product.cart&.persisted?
-  #     @cart = @product.cart
-  #   else
-  #     @cart = Cart.new(product_id: params[:id], user_id: current_user.id, quantity: params[:cart][:quantity])
-  #     # session[:cart_id] = @cart.id
-  #   end
-  # end
-
   protected
 
   def configure_permitted_parameters
