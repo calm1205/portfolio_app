9fad49a31b0b4b365da98d29f6acfa5f87feb4d8
calm1205 <naginani072@gmail.com>
2020-11-22 22:01:39 +0900

    cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,37 +1,53 @@
 class CartsController < ApplicationController
   before_action :cart_create_or_find
+
   def index
-    @products = @cart.products
+    @total_price   = @cart.total_price
+    @cart_products = @cart.cart_products.includes(:product)
+    @card          = Card.get_card(current_user)
+    @address       = current_user.address
   end
 
   def cart_in
     product = Product.find(params[:product_id])
+    result  = true
     if @cart.products.include?(product)
-      @cart.update_sum_same_product_quantities(product.id, params[:quantity].to_i)
+      result = false unless @cart.update_quantities(product.id, params[:quantity].to_i)
     else
       @cart_product = @cart.cart_products.build(product_id: params[:product_id], quantity: params[:quantity])
-      unless @cart_product.save
-        redirect_to root_path
-        flash[:notice] = "商品をカートに入れることができませんでした。"
-      end
+      result = false unless @cart_product.save
     end
-    flash[:notice] = "商品をカートに入れました。"
+    render json: { result: result }
   end
 
-  def destroy
+  # cartからの削除
+  def drop
       @cart_product = CartProduct.find(params[:id])
-      @cart_product.destroy
-      redirect_to carts_path, notice: "カートの商品を削除しました"
+      if @cart_product.destroy
+        render json: { result: true }
+      else
+        render json: { result: false }
+      end
   end
 
+  # cartから+/-ボタンで商品数変更
+  def quantity_edit
+    cart_product = CartProduct.find(params[:cart_id])
+    if cart_product.quantity_updown(params[:up_down])
+      render json: { result: true }
+    else
+      render json: { result: false }
+    end
+  end
 
-  private 
+  private
 
   def cart_create_or_find
-    if !current_user.cart
-      @cart = Cart.create(user_id: current_user.id)
-    else
+    if current_user.cart
       @cart = Cart.find_by(user_id: current_user.id)
+    else
+      @cart = Cart.create(user_id: current_user.id)
     end
   end
+
 end
