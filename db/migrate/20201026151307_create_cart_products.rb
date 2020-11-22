75e0d236df65cbb80187061b6dc14d3d599a3f55
calm1205 <naginani072@gmail.com>
2020-11-22 22:01:49 +0900

    cart db

--- db/migrate/20201026151307_create_cart_products.rb
+++ db/migrate/20201026151307_create_cart_products.rb
@@ -3,7 +3,7 @@ class CreateCartProducts < ActiveRecord::Migration[6.0]
     create_table :cart_products do |t|
       t.references :product, null: false, foreign_key: true
       t.references :cart, null: false, foreign_key: true
-      t.integer :quantity
+      t.integer    :quantity
       t.timestamps
     end
   end
