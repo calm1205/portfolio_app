a63f2e50be0f1724185a90451820c14a88d0c871
calm1205 <naginani072@gmail.com>
2021-03-01 19:52:58 +0900

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'portfolio_app'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Githubのユーザー名/レポジトリ名.git'
+set :repo_url,  'git@github.com:calm1205/portfolio_app.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
