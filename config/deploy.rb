aca226ac4aa2a7f6eee1cd71a24e8af825538f64
calm1205 <naginani072@gmail.com>
2021-03-01 20:02:34 +0900

    fix header logo image

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/portfolio.pem'] 
+set :ssh_options, auth_methods: ['publickey'],keys: ['~/.ssh/portfolio.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
