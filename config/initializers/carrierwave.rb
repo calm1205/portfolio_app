c429db19b5af3c0a5886c57ba700126120d8b43c
calm1205 <naginani072@gmail.com>
2021-03-02 10:53:34 +0900

    save location change

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,6 @@
 CarrierWave.configure do |config|
 
-  # config.fog_public = false
-
   if Rails.env.production?
-    config.root = "#{Rails.root}/public"
+    # config.root = "#{Rails.root}/public"
   end
 end
