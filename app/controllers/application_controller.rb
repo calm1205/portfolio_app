1c41f8035f97980baf96395c465d1f6b93836213
calm1205 <naginani072@gmail.com>
2021-03-05 15:01:20 +0900

    basic auth setting and reset debug config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+  # before_action :basic_auth
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :configure_account_update_parameters, if: :devise_controller?
 
   protected
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic[:user_name] && password == Rails.application.credentials.basic[:password]
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
